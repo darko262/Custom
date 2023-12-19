8f0169e0f8f86ee9923f3ade0cb5a60dbca33ee5
darko262 <s.padilla01@ufromail.cl>
2023-12-19 16:11:55 -0300

    mejoramiento 16:11 19/12/23

--- core/urls.py
+++ core/urls.py
@@ -23,6 +23,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/blog/', include('apps.blog.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
